c3658cf07590cc877c26aa30c3c4cb76ffa8f62c
Crowdbotics <team@crowdbotics.com>
2021-08-20 19:42:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "winter_darkness_29987Identifier",
+  url: "https://winter_darkness_29987Identifier.botics.co",
+  api: "https://winter_darkness_29987Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
